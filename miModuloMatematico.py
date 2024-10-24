0a68a70a1f37bc7ecc253822a0ffac56237a8b25
Alejandro Reyes <caballeroandante1408@gmail.com>
2024-10-24 19:12:32 -0400

    viendo un poco de modulos con python

---
+++ miModuloMatematico.py
@@ -0,0 +1,8 @@
+def multiplicar(a,b):
+    return a * b
+
+def suma(a,b):
+    return a + b
+
+def resta(a,b):
+    return a - b
---
+++ modulos.py
@@ -0,0 +1,20 @@
+# Modulos en pyhton 
+
+import math
+import miModuloMatematico as mat
+
+raiz = math.sqrt(25)
+
+print(raiz) # 4
+
+multiplicar = mat.multiplicar(4,5)
+
+print("El resultado del producto es: ",multiplicar)
+
+sumar = mat.suma(45,5)
+
+print("El resultado de la suma es: ", sumar)
+
+restar = mat.resta(80,40)
+
+print("El resultado de la resta es: ",restar)
